0a5805281df71fbc48f3f997e2b261cc1f027f99
Anastasia <anastasia.paradizova@icloud.com>
2024-09-24 20:45:11 +0400

    Doodle jump

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from player import Player
 from platforma import *
 import random
 
+with open('record.txt', 'r', encoding='utf-8') as file:
+    record = int(file.readline())
 
 pygame.init()
 screen = pygame.display.set_mode((470, 550))
@@ -11,7 +13,7 @@ running = True
 pr = Player()
 pl = [Platform() for _ in range(10000)]
 ply = 500
-
+font = pygame.font.Font(None, 30)
 
 for p in pl:
     ply -= 70
@@ -24,14 +26,24 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         pr.control(event);
-
+    if pr.y - pr.min_y > 300:
+        running = False
     pr.fall_move(platforms=pl)
     screen.fill('black')
 
     pr.draw(screen)
     for i in pl:
-        scroll = 200 - pr.y
+        scroll = 200 - pr.min_y
         i.draw(screen, scroll)
+
+    text = font.render(f'Высота - {-int(pr.min_y)}', True, (255, 255, 255))
+    text_record = font.render(f'Рекорд: {record}', True, (255, 255, 255))
+
+    screen.blit(text, (20, 10))
+    screen.blit(text_record, (330, 10))
 
     pygame.display.flip()
 pygame.quit()
+if -int(pr.min_y) > record:
+    with open('record.txt', 'w', encoding='utf-8') as file:
+        file.write(str(-int(pr.min_y)))
--- player.py
+++ player.py
@@ -13,6 +13,7 @@ class Player:
         print("Player created")
         self.x = 150
         self.y = 50
+        self.min_y = self.y
         self.xr = 50
         self.yr = 56
         self.s = 0
@@ -27,16 +28,17 @@ class Player:
             self.tex[i] = pygame.transform.scale(self.tex[i], (self.xr, self.yr))
 
     def draw(self, screen):
+        y = 200 + (self.y - self.min_y)
         if time.perf_counter() - self.time > 0.2:
             if self.dir == 0:
-                screen.blit(self.tex[1], (self.x, 200))
+                screen.blit(self.tex[1], (self.x, y))
             elif self.dir == 1:
-                screen.blit(self.tex[2], (self.x, 200))
+                screen.blit(self.tex[2], (self.x, y))
         else:
             if self.dir == 0:
-                screen.blit(self.tex[4], (self.x, 200))
+                screen.blit(self.tex[4], (self.x, y))
             elif self.dir == 1:
-                screen.blit(self.tex[3], (self.x, 200))
+                screen.blit(self.tex[3], (self.x, y))
 
     def move(self, dx, dy):
         self.x += dx
@@ -44,26 +46,27 @@ class Player:
 
     def control(self, event):
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
+            if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
                 self.dir = 0
                 self.right = True
-            if event.key == pygame.K_LEFT:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_a:
                 self.dir = 1
                 self.left = True
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
+            if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
                 self.right = False
-            if event.key == pygame.K_LEFT:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_a:
                 self.left = False
 
     def fall_move(self, platforms):
         for pl in platforms:
             if collision(self.x, self.y, self.xr, self.yr, pl.x, pl.y, pl.xr, pl.yr) and self.s > 0:
-                self.s = -180
+                self.s = -200
                 self.time = time.perf_counter()
 
         self.y += self.s * (time.perf_counter() - self.t)
-        self.s += 40 * (time.perf_counter() - self.t)
+        self.min_y = min(self.y, self.min_y)
+        self.s += 46 * (time.perf_counter() - self.t)
         self.t = time.perf_counter()
 
         if self.right:
